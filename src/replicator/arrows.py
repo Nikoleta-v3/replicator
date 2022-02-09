4215c3bb36879adc55c77cc1071c96b7503e45c9
Nikoleta-v3 <glynatsi@evolbio.mpg.de>
2022-02-09 18:54:53 +0100

    adds a file to plot arrows in 3D
    This code has not been implemented by me

---
+++ src/replicator/arrows.py
@@ -0,0 +1,34 @@
+"""Original code: https://gist.github.com/WetHat/1d6cd0f7309535311a539b42cccca89c.
+This code has been developed by @WetHat."""
+
+from mpl_toolkits.mplot3d.proj3d import proj_transform
+from matplotlib.patches import FancyArrowPatch
+
+from matplotlib.text import Annotation
+
+
+class Annotation3D(Annotation):
+    def __init__(self, text, xyz, *args, **kwargs):
+        super().__init__(text, xy=(0, 0), *args, **kwargs)
+        self._xyz = xyz
+
+    def draw(self, renderer):
+        x2, y2, z2 = proj_transform(*self._xyz, self.axes.M)
+        self.xy = (x2, y2)
+        super().draw(renderer)
+
+
+class Arrow3D(FancyArrowPatch):
+    def __init__(self, x, y, z, dx, dy, dz, *args, **kwargs):
+        super().__init__((0, 0), (0, 0), *args, **kwargs)
+        self._xyz = (x, y, z)
+        self._dxdydz = (dx, dy, dz)
+
+    def draw(self, renderer):
+        x1, y1, z1 = self._xyz
+        dx, dy, dz = self._dxdydz
+        x2, y2, z2 = (x1 + dx, y1 + dy, z1 + dz)
+
+        xs, ys, zs = proj_transform((x1, x2), (y1, y2), (z1, z2), self.axes.M)
+        self.set_positions((xs[0], ys[0]), (xs[1], ys[1]))
+        super().draw(renderer)
